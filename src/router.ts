7cc236f7040ada089b32f5509f0769ddbd176b72
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-05-30 21:03:35 +0300

    fix: add CORS handling (#15)

--- src/router.ts
+++ src/router.ts
@@ -4,19 +4,50 @@ import OrcaInfoCache from "./kv";
 
 const router = Router();
 
-router.get("/info", async () => {
+const getCorsHeaders = (request: Request) => {
+  const headers = new Headers();
+
+  try {
+    const url = new URL(request.headers.get("Origin") as string);
+    const whiteList = ["localhost:3000", "wallet.p2p.org", "p2p-org.github.io"];
+
+    if (whiteList.includes(url.host)) {
+      headers.set(
+        "Access-Control-Allow-Origin",
+        url.toString().replace(/\/$/, "")
+      );
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  return headers;
+};
+
+router.get("/info", async (request: Request) => {
   const orcaInfo = await OrcaInfoCache.getInfo();
 
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  return new Response(JSON.stringify(orcaInfo, null, 2));
+  return new Response(JSON.stringify(orcaInfo, null, 2), {
+    headers: getCorsHeaders(request),
+  });
 });
 
-router.get("/meta", async () => {
+router.get("/meta", async (request: Request) => {
   const cacheMeta = await OrcaInfoCache.getCacheMeta();
 
-  return new Response(JSON.stringify(cacheMeta));
+  return new Response(JSON.stringify(cacheMeta), {
+    headers: getCorsHeaders(request),
+  });
 });
 
-router.all("*", () => new Response("Not Found.", { status: 404 }));
+router.all(
+  "*",
+  (request: Request) =>
+    new Response("Not Found.", {
+      status: 404,
+      headers: getCorsHeaders(request),
+    })
+);
 
 export { router };
